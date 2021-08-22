9b391709135aaa797db88316c99cc03fa68dd703
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-08-22 15:52:38 -0300

    implement dynamic allocation to the vector

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM_MAX 5
+#define TAM_MAX 6
 
+// Ask the vector value and return
+// the dinamic allocation
 int print_vetor(int vet[], int tamanho)
 {
   int counter = 0;
   printf("{ ");
-  for (int i; i < tamanho; i++)
+  for (int i = 0; i < tamanho; i++)
   {
     if (vet[i] != 0)
     {
@@ -25,9 +27,8 @@ int print_vetor(int vet[], int tamanho)
   return counter;
 }
 
-int main()
+int pedir_vetor(int *vetor)
 {
-  int numeros[TAM_MAX] = {};
 
   for (int i = 0; i < TAM_MAX; i++)
   {
@@ -37,12 +38,12 @@ int main()
     }
 
     printf("Digite um valor: ");
-    scanf("%d", &numeros[i]);
+    scanf("%d", &vetor[i]);
 
-    if (numeros[i] == 0)
+    if (vetor[i] == 0)
       break;
 
-    if (numeros[i] < 0 || numeros[i] > 100)
+    if (vetor[i] < 0 || vetor[i] > 100)
     {
       printf("Esse valor nao está no intervalo!\n");
       printf("Digite novamente!\n\n");
@@ -50,8 +51,27 @@ int main()
     }
   }
 
-  int result = print_vetor(numeros, TAM_MAX);
-  printf("Valores inseridos: %d\n", result);
+  int qnt_inputs = print_vetor(vetor, TAM_MAX);
+  printf("Valores inseridos: %d\n", qnt_inputs);
+
+  return qnt_inputs;
+}
+
+int main()
+{
+  int *vetor = (int *)malloc(TAM_MAX * sizeof(int));
+  int qnt_input = pedir_vetor(vetor);
+  int *vetor_inputs = (int *)malloc(qnt_input * sizeof(int));
+  
+  for (int i = 0; i < qnt_input; i++)
+  {
+    vetor_inputs[i] = vetor[i];
+  }
+  
+  free(vetor);
+  
+  print_vetor(vetor_inputs, qnt_input);
+
 
   return 0;
 }
